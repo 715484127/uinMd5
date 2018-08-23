e987ba0b8b6b1486665d7660d1c5ee3dead091b6
715484127 <715484127@qq.com>
2018-08-23 09:15:27 +0800

    1

--- getUinForHttp.py
+++ getUinForHttp.py
@@ -7,7 +7,9 @@ import logging
 # 配置日志
 logname = "uin.log"
 filehandler = logging.FileHandler(filename=logname, encoding="utf-8")
-fmter = logging.Formatter(fmt="%(asctime)s.%(msecs)03d %(filename)s[line:%(lineno)d] %(levelname)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+fmter = logging.Formatter(
+    fmt="%(asctime)s.%(msecs)03d %(filename)s[line:%(lineno)d] %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S")
 filehandler.setFormatter(fmter)
 loger = logging.getLogger(__name__)
 loger.addHandler(filehandler)
@@ -44,44 +46,54 @@ def get_task():
 
 
 # 根据MD5值获取uin
-@app.route('/get_uin/', methods=['GET'])
+@app.route('/get_uin', methods=['GET'])
 def get_uin():
     time_start = time.time()
     if not request.args or 'md5' not in request.args:
         abort(400)
     else:
         result = ()
-        md5 = request.args['md5']
-        db = pymysql.connect("localhost", "root", "2006wang", "uin")
-        cursor = db.cursor()
-        for data in range(0, 80):
-            if data <= 9:
-                data = '0' + str(data)
-            table_name = str(data)
-            sql = """select uin from `""" + table_name + \
-                """` where md5 =  '""" + md5 + """'"""
-            cursor.execute(sql)
-            result = cursor.fetchone()
-            if not result:
-                print(
-                    "table_name:" +
-                    table_name +
-                    "|#MD5:" +
-                    md5 +
-                    "|#没有对应的UIN")
-            else:
-                print(
-                    "table_name:" +
-                    table_name +
-                    "|#MD5:" +
-                    md5 +
-                    "|#UIN:" +
-                    result[0])
-                break
+        result_json = {'uin': 'not found'}
+        md5 = request.args['md5'].upper()
+        loger.info("|#请求参数|#MD5=" + md5)
+        if not md5:
+            loger.info("|#请求参数|#MD5=" + md5 + "|#请求参数错误")
+        else:
+            db = pymysql.connect("localhost", "root", "2006wang", "uin")
+            cursor = db.cursor()
+            for data in range(0, 80):
+                if data <= 9:
+                    data = '0' + str(data)
+                table_name = str(data)
+                sql = """select uin from `""" + table_name + \
+                      """` where md5 =  '""" + md5 + """'"""
+                cursor.execute(sql)
+                result = cursor.fetchone()
+                if not result:
+                    loger.info(
+                        "|#table_name:" +
+                        table_name +
+                        "|#MD5:" +
+                        md5 +
+                        "|#没有对应的UIN")
+                else:
+                    loger.info(
+                        "|#table_name:" +
+                        table_name +
+                        "|#MD5:" +
+                        md5 +
+                        "|#UIN:" +
+                        result[0])
+                    break
         time_end = time.time()
-        print("|#查询总耗时:%.2f秒|#MD5=%s|#UIN=%s" %
-              ((time_end - time_start), md5, result[0]))
-        return jsonify(result) if result else jsonify({'result': 'not found'})
+        if not result:
+            result = ()
+            loger.info("|#查询总耗时:%.2f秒|#MD5=%s|#UIN=%s|#没有查询到UIN" % ((time_end - time_start), md5, ''))
+        else:
+            result_json.update(uin=result[0])
+            loger.info("|#查询总耗时:%.2f秒|#MD5=%s|#UIN=%s" % ((time_end - time_start), md5, result[0]))
+
+        return jsonify(result_json) if result else jsonify({'uin': 'not found'})
 
 
 if __name__ == "__main__":
